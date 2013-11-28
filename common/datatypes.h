44fc9bd9b3e9c88aebe809894bbe07bb64e3505d
Matej Vavrinec <wmatex@gmail.com>
2013-11-28 14:17:16 +0100

    + message type changed from enum to simplier #define + structure for message as defined by protocol

--- common/datatypes.h
+++ common/datatypes.h
@@ -1,15 +1,27 @@
 /*
+ * Datatypes used by client and server
  */
 #ifndef DATATYPES_H
 #define DATATYPES_H
 
-typedef enum __message_type_s {
-  LOGIN,
-  DISCONN,
-  TEXT,
-  FATAL_ERROR,
-  SOFT_ERROR,
-  OK
-} message_type_t;
+#include <ao/os_types.h>
+
+#define MESSAGE_TYPE_LOGIN       0
+#define MESSAGE_TYPE_DISCONN     1
+#define MESSAGE_TYPE_TEXT        2
+#define MESSAGE_TYPE_OK          3
+#define MESSAGE_TYPE_SOFT_ERROR  4
+#define MESSAGE_TYPE_FATAL_ERROR 5
+
+/* 1 byte for type and 4 bytes for id */
+#define MSG_PADDING 5
+
+/* Message struct */
+typedef struct __message_s {
+  uint_8 type;     /* Type of message (one of MESSAGE_TYPE_*) */
+  uint_32 id;      /* Message id */
+  char *msg;       /* Actual message */
+  size_t msg_len;  /* Message length */
+} message_t;
 
 #endif /* end of include guard: DATATYPES_H */

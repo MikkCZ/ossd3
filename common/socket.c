8aa2f177541065e3d41882dfc44d01e566495d1d
Matej Vavrinec <wmatex@gmail.com>
2013-11-28 14:20:35 +0100

    new functions for sending/receiving whole protocol message

--- common/socket.c
+++ common/socket.c
@@ -1,7 +1,14 @@
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <ao/os_types.h>
 #include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
 #include "socket.h"
+
+#include "common/error.h"
+
+#define BUFLEN 1024
 
 void *get_in_addr(struct sockaddr *sa)
 {
@@ -9,4 +16,113 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in*)sa)->sin_addr);
   }
   return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int mesg_recv(int socket, message_t **msg) {
+  size_t len, received;
+  char buf[BUFLEN];
+  char *msg_buf;
+
+  /* Receive the first part */
+  if ((len = recv(socket, (void *)buf, BUFLEN-1, 0)) < 0) {
+    return len;
+  }
+
+  received = len - 2;
+  size_t msg_size;
+  /* Convert first 2 bytes to number in host format */
+  msg_size = ntohs(*((uint_16 *)buf));
+
+  msg_buf = (char *) calloc(msg_size, sizeof(char));
+  if (msg_buf == NULL) {
+    print_error("memory allocation failed");
+    return -1;
+  }
+  /* Copy the received buffer to the message excluding the 2 starting bytes */
+  memcpy(msg_buf, &buf[2], len-2);
+
+  /* Keep receiving until we got the whole message */
+  while (received < msg_size) {
+    if ((len = recv(socket, (void *)&msg_buf[received], msg_size-received, 0)) <= 0) {
+      free(msg_buf);
+      return len;
+    }
+    received += len;
+  }
+
+  /* Create the structure, that will hold the message */
+  message_t *mesg_s;
+  if ((mesg_s = (message_t *) calloc(1, sizeof(message_t))) == NULL) {
+    free(msg_buf);
+    print_error("memory allocation failed");
+    return -1;
+  }
+
+  /* Message type (1 byte) */
+  mesg_s->type = *((uint_8 *) msg_buf);
+
+  /* Message id (4 bytes) */
+  mesg_s->id = ntohl(*((uint_32 *) &msg_buf[1]));
+
+  /* Actual message */
+  mesg_s->msg_len = msg_size - MSG_PADDING;
+  if ((mesg_s->msg = (char *) malloc((mesg_s->msg_len + 1) * sizeof(char))) == NULL) {
+    free(msg);
+    free(mesg_s);
+    print_error("memory allocation failed");
+    return -1;
+  }
+  memcpy(mesg_s->msg, &msg_buf[MSG_PADDING], mesg_s->msg_len);
+  mesg_s->msg[mesg_s->msg_len] = (char)0;
+
+  /* Update the pointer */
+  *msg = mesg_s;
+
+  /* Return success */
+  return msg_size;
+}
+
+int mesg_send(int socket, uint_8 type, 
+    uint_32 id, const char *msg, int can_fail) {
+  if (can_fail > 0 && (double)random()/RAND_MAX < FAIL_PROBABILITY) {
+    /* Fail silently giving user no feedback about the error (packet is lost) */
+    return 1;
+  }
+
+  size_t m_len = strlen(msg);
+  uint_16 msg_size = MSG_PADDING+m_len;
+  size_t actual_length = msg_size+2; /* Length of the packet + size */
+  printf("Length = %d\n", actual_length);
+  char buffer[actual_length+2];
+
+  uint_16 msg_size_net = htons(msg_size);
+  char *c = (char *)&msg_size_net;
+  /* Serialize the message size to char array */
+  buffer[0] = *c; buffer[1] = *(c+1);
+  
+  /* Send the message type */
+  buffer[2] = (char)type;
+
+  /* Message id */
+  uint_32 msg_id_net = htonl(id);
+  c = (char *)&msg_id_net;
+  /* Serialize the message id */
+  int i;
+  for (i = 0; i < 4; i++) {
+    buffer[3+i] = *(c+i);
+  }
+
+  /* Actual message */
+  memcpy(&buffer[2+MSG_PADDING], msg, m_len);
+
+  int len, sent = 0;
+  /* Ensure the whole message is sent */
+  do {
+    if ((len = send(socket, (void *)&buffer[sent], actual_length - sent, 0)) == -1) {
+      return -1;
+    }
+    sent += len;
+  } while(sent < actual_length);
+
+  return 1;
 }
--- common/socket.h
+++ common/socket.h
@@ -4,7 +4,24 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
+#include "common/datatypes.h"
+
+#define FAIL_PROBABILITY 0.3
+
 /* Get IP address based on protocol version (IPv4 or IPv6) */
 void *get_in_addr(struct sockaddr *sa);
+
+/* Receive the whole message */
+int mesg_recv(int socket, message_t **msg);
+
+/* Send message as defined by the protocol
+ * if can_fail > 0: message will fail to send with defined probability */
+int mesg_send(int socket, uint_8 type, 
+    uint_32 id, const char *msg, int can_fail);
+
+/* Keep sending message until other side sends confirmation */
+/* TODO: implement it! */
+int conf_mesg_send(int socket, uint_8 type,
+    uint_32 id, const char *msg, int can_fail);
 
 #endif /* end of include guard: SOCKET_H */
